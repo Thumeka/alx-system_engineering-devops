da7ea22b746fe5280407d3b9e55e62cb566bb71e
thumeka <thumekatembani21@gmail.com>
2023-11-24 05:14:17 -0800

    problems

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,12 @@
 #Using Puppet, create a file in /tmp.
 
+#Using Puppet, create a file in /tmp.
+
 file { 'school':
-ensure 	=> 'present',
-content => 'Puppet is cool',
-path	=> '/tmpw/school',
-mode	=> '0744',
-owner 	=> 'www-data',
-group	=> 'www-data',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
