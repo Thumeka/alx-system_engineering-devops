0c8836460d7e35fe99f49b2616fe1e0bc3c9758c
thumeka <thumekatembani21@gmail.com>
2024-01-17 06:10:29 -0800

    web

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,16 +15,13 @@ def number_of_subscribers(subreddit):
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     user_agent = {'User-agent': 'Google Chrome Version 120.0.6099.217'}
 
-    try:
-        response = get(api_url, headers=user_agent, allow_redirects=False)
+    response = get(api_url, headers=user_agent, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
 
-        if response.status_code == 200:
-            results = response.json()
-            subscribers = results['data']['subscribers']
-            return subscribers
-        else:
-            print(f"Error: {response.status_code}")
-            return 0
-    except Exception as e:
-        print(f"Exception: {e}")
+    results = response.json()
+    try:
+        return results['data']['subscribers']
+
+    except Exception:
         return 0
