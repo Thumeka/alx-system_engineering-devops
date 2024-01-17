86626c3cac28fdd1d3356d8f24c61ca8ebf2074f
thumeka <thumekatembani21@gmail.com>
2024-01-17 09:46:40 -0800

    count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-"""Module for advance API exercices"""
+"""Module for advance API exercises"""
 import requests
 
 url = 'http://reddit.com/r/{}/hot.json'
 
-def count_words(subreddit, word_list, after=None, res={}):
-    """Fetchers all hot posts of a subreddit"""
-    headers = {"User-Agent": "Google Chrome Version 120.0.6099.225"}
-    params = {'limit': 100}
-    if isinstance(after, str):
-        if after != 'null':
-            params['after'] = after
-        else:
-            res = res = sorted(res.items(), key=lambda x: x[1], reverse=True)
-            sorted_dict = {k: v for k, v in res}
-            for k in sorted_dict:
-                print("{}: {}".format(k, sorted_dict[k]))
-            return
-    response = requests.get(url.format(subreddit),
-                            headers=headers,
-                            params=params)
+def count_words(subreddit, word_list, after=None, res=None):
+    """Fetches all hot posts of a subreddit"""
+    if res is None:
+        res = {}
+
+    headers = {'User-Agent': 'Google Chrome Version 120.0.6099.225'}
+    params = {'limit': 100, 'after': after} if after else {'limit': 100}
+
+    response = requests.get(url.format(subreddit), headers=headers, params=params)
+
     if response.status_code != 200:
-        print()
-        return None
+        print(f"Error: {response.status_code}")
+        return
+
     for elem in response.json()['data']['children']:
         title = elem['data']['title'].lower().split()
         for word in word_list:
             if word in title:
-                if word in res:
-                    res[word] += 1
-                else:
-                    res[word] = 1
+                res[word] = res.get(word, 0) + 1
+
     after = response.json()['data']['after']
-    if after is None:
-        after = 'null'
-    return count_words(subreddit, word_list, after, res)
+    if after:
+        return count_words(subreddit, word_list, after, res)
+
+    sorted_res = sorted(res.items(), key=lambda x: (-x[1], x[0]))
+    for k, v in sorted_res:
+        print(f"{k}: {v}")
